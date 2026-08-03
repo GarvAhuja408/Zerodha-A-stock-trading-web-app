72aeb6c37c899472665888fb3a13bfb09bc444e6
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-08-03 16:22:47 +0530

    Education component setup

--- frontend/src/landing_page/home/Education.js
+++ frontend/src/landing_page/home/Education.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 function Education() {
     return ( 
-        <h1>Education</h1>
+        <div className='container mt-5'>
+            <div className='row mt-5'>
+
+                <div className='col-6'>
+                    <img src="media/varsityy.png" alt='varsity image'/>
+                </div>
+
+                <div className='col-6'>
+                    <h1 className='mt-5'>Free and open market education</h1>
+
+                    <p className='mt-5'>Varsity, the largest online stock market education book in the world covering everything from the basics to advanced trading.</p>
+                    <a href='/'>Varsity</a>
+
+                    <p className='mt-4'>TradingQ&A, the most active trading and investment community in India for all your market related queries.</p>
+                    <a href='/'>TradingQ&A</a>
+                </div>
+            </div>
+        </div>
      );
 }
 
