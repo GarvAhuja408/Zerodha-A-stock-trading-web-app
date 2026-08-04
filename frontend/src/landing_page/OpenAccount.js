3961cc6a9b77e6415f4da980e54578f45222d2ae
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-08-04 15:31:03 +0530

    OA component setup

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 function OpenAccount() {
     return ( 
-        <h1>Open Account</h1>
+        <div className='container p-4'>
+            <div className="row text-center">
+
+                <h1 className='mt-5'>Open a Zerodha account</h1>
+                <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+                <button style={{width:"25%",margin:"0 auto"}} type="button" className="btn btn-primary p-3 mb-5">Sign up for free</button>
+                
+            </div>
+        </div>
      );
 }
 
