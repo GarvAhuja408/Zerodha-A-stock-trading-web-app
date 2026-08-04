a58b3cd44146e243d76ee9c559a6e035a9fdf2bb
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-08-04 15:55:51 +0530

    Footer setup

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -6,9 +6,9 @@ function OpenAccount() {
             <div className="row text-center">
 
                 <h1 className='mt-5'>Open a Zerodha account</h1>
-                <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
-                <button style={{width:"25%",margin:"0 auto"}} type="button" className="btn btn-primary p-3 mb-5">Sign up for free</button>
-                
+                <p className='mt-3'>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+                <button style={{width:"25%",margin:"0 auto"}} type="button" className="btn btn-primary p-3 mt-3 mb-5">Sign up for free</button>
+
             </div>
         </div>
      );
