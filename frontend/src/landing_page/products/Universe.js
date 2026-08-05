4ce651fd617ccb6c9b4dac316d6fc882f6c0e2f4
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-08-05 16:07:13 +0530

    Universe component setup

--- frontend/src/landing_page/products/Universe.js
+++ frontend/src/landing_page/products/Universe.js
@@ -2,7 +2,63 @@ import React from 'react';
 
 function Universe() {
     return (  
-        <h1>Universe</h1>
+        <div className='container mt-5 text-muted'>
+                <div className='row text-center'>
+
+                    <h1>The Zerodha Universe</h1>
+
+                    <p>Extend your trading and investment experience even further with our partner platforms</p>
+
+                    <div className='col-4 p-5'>
+                        <img src="/media/one.png"/>
+                        <p>Our asset management venture
+                        that is creating simple and transparent index
+                        funds to help you save for your goals.
+                        </p>
+                    </div>
+
+                    <div className='col-4 p-5'>
+                        <img src="/media/2.png"/>
+                        <p>Options trading platform that lets you
+                        create strategies, analyze positions, and examine
+                        data points like open interest, FII/DII, and more.
+                        </p>
+                    </div>
+
+                    <div className='col-4 p-5'>
+                        <img src="/media/3.png"/>
+                        <p>Investment research platform
+                        that offers detailed insights on stocks,
+                        sectors, supply chains, and more.</p>
+                    </div>
+
+                    <div className='col-4 p-5'>
+                        <img src="/media/4.png"/>
+                        <p>Systematic trading platform
+                        that allows you to create and backtest
+                        strategies without coding.</p>
+                    </div>
+
+
+                    <div className='col-4 p-5'>
+                        <img src="/media/5.png"/>
+                        <p>Thematic investing platform
+                        that helps you invest in diversified
+                        baskets of stocks on ETFs.</p>
+                    </div>
+
+                    <div className='col-4 p-5'>
+                        <img src="/media/6.png"/>
+                        <p>Personalized advice on life
+                        and health insurance. No spam
+                        and no mis-selling.
+                        Sign up for free</p>
+                    </div>
+
+                    <button style={{width:"20%",margin:"0 auto"}} type="button" className="btn btn-primary p-3 mb-5 text-lg">Signup Now</button>
+
+                </div>
+        </div>
     );
 }
 
