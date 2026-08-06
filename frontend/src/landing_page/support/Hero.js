2466485441e5fda2e311a480686078008a5e547f
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-08-06 15:56:41 +0530

    Support component

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -2,7 +2,39 @@ import React from 'react';
 
 function Hero() {
     return ( 
-        <h1>CreateTicket</h1>
+        <section className='container-fluid' id='support-hero'>
+
+            <div className= 'p-5' id="supportWrapper">
+                <h4 style={{color:"white"}}>Support Portal</h4>
+                <a href="/" style={{color:"white"}}>Track Tickets</a>
+            </div>
+
+            <div className= 'row p-5 ' style={{color:"white"}}>
+
+                <div className='col-6 p-5' >
+                    <h2 className='fs-3'>Search for an answer or browse help topics to create a ticket</h2>
+                    <input placeholder='Eg. how do i activate my F&O'></input>
+                    <br></br>
+                    <a href="/" style={{color:"white"}}>Track Account opening</a>
+                    <a href="/" style={{color:"white"}}>Intraday margin</a>
+                    <a href="/" style={{color:"white"}}>Kite User Manual</a>
+                    <a href="/" style={{color:"white"}}>Track Segment activation</a>
+                </div>
+
+                <div className='col-6 p-5'>
+                    <h2>Featured</h2>
+
+                    <ol>
+                        <li><a href="/" style={{color:"white"}}>Current takeover and delisting</a></li>
+     
+                    <li><a href="/" style={{color:"white"}}>Latest intraday leverage</a></li>
+                    </ol>
+                </div>
+
+
+            </div>
+
+        </section>
      );
 }
 
--- frontend/src/landing_page/support/SupportPage.js
+++ frontend/src/landing_page/support/SupportPage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Hero from './Hero.js';
 import CreateTicket from './CreateTicket';
 
+
 function SupportPage() {
     return ( 
         <div>
+            <Hero/>
             <CreateTicket/>
         </div>
      );
 }
 
-export default CreateTicket;
+export default SupportPage;
