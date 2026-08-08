f53a1d36330cd867bae93df4801538deb258b1dc
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-08-08 15:32:15 +0530

    added data and  made  holdings dynamic acc to holdings

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -14,7 +14,9 @@ import WatchList from "./WatchList";
 const Dashboard = () => {
   return (
     <div className="dashboard-container">
-        <WatchList />
+
+      <WatchList />
+
       <div className="content">
         <Routes>
           <Route exact path="/" element={<Summary />} />
--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { holdings } from "../data/data.js";
 
 const Holdings = () => {
   return (
     <>
-      <h3 className="title">Holdings (13)</h3>
+      <h3 className="title">Total Holdings - ({holdings.length})</h3>
 
       <div className="order-table">
         <table>
+
           <tr>
             <th>Instrument</th>
             <th>Qty.</th>
@@ -17,6 +19,27 @@ const Holdings = () => {
             <th>Net chg.</th>
             <th>Day chg.</th>
           </tr>
+
+          {holdings.map( (stock,index)=>{
+
+            const currVal = stock.price*stock.qty;
+            const isProfit = currVal-stock.avg*stock.qty>=0.0;
+            const profClass = isProfit ?"profit":"loss";
+            const dayClass = stock.isLoss ?"loss":"profit";
+
+            return (
+              <tr key={index}>
+                <td>{stock.name}</td>
+                <td>{stock.qty}</td>
+                <td>{stock.avg.toFixed(2)}</td>
+                <td>{stock.price.toFixed(2)}</td>
+                <td>{currVal}</td>
+                <td className={profClass}>{(currVal-stock.avg+stock.qty).toFixed(2)}</td>
+                <td className={profClass}>{stock.net}</td>
+                <td className={profClass}>{stock.day}</td>
+              </tr>
+            )
+          })}
         </table>
       </div>
 
