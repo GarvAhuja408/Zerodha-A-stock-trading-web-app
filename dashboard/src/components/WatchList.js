52e1def32f1e6398ca3d82f1c5be6ed201cdf832
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-08-09 15:35:16 +0530

    Used material UI to adjust watchlist components according to price

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -2,6 +2,7 @@ import React,{useState} from "react";
 
 // after material UI  npm install @mui/icons-material @mui/material @emotion/styled @emotion/react
 import {Tooltip,Grow} from '@mui/material';
+import {KeyboardArrowDown,KeyboardArrowUp} from '@mui/icons-material';
 
 import {watchlist} from '../data/data.js';
 
@@ -24,8 +25,9 @@ const WatchList = () => {
       <ul className="list">
 
           {watchlist.map( (stock,index)=>{
+            return(
               <WatchlistItem stock={stock} key={index} />
-            })}
+            )})}
 
       </ul>
 
@@ -34,3 +36,30 @@ const WatchList = () => {
 };
 
 export default WatchList;
+
+
+const WatchlistItem = ({stock,key})=>{
+
+    const[showWatchlistActions,setShowWatchlistActions]=useState(false);
+
+    const handleMouseEnter = (e)=>{
+        setShowWatchlistActions(true);
+    }
+
+    const handleMouseExit = (e)=>{
+        setShowWatchlistActions(false);
+    }
+    
+    return(
+      <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseExit}>
+          <div className="item">
+              <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
+              <div className="itemInfo">
+                <span className="percent">{stock.percent}</span>
+                {stock.isDown ? ( <KeyboardArrowDown className="down"/> ) : ( <KeyboardArrowUp className="up"/> )}
+                <span className="percent">{stock.price}</span>
+              </div>
+          </div>
+      </li>
+    )
+}
