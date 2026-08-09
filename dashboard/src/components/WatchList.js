addae1df2ce06e6b7f5dea25c2c719a90871bd46
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-08-09 15:17:53 +0530

    Added watchlistItem and made it dynamic

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,4 +1,11 @@
-import React from "react";
+import React,{useState} from "react";
+
+// after material UI  npm install @mui/icons-material @mui/material @emotion/styled @emotion/react
+import {Tooltip,Grow} from '@mui/material';
+
+import {watchlist} from '../data/data.js';
+
+
 
 const WatchList = () => {
   return (
@@ -11,10 +18,17 @@ const WatchList = () => {
           placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
           className="search"
         />
-        <span className="counts"> 9 / 50</span>
+        <span className="counts"> {watchlist.length} / 50</span>
       </div>
 
-      <ul className="list"></ul>
+      <ul className="list">
+
+          {watchlist.map( (stock,index)=>{
+              <WatchlistItem stock={stock} key={index} />
+            })}
+
+      </ul>
+
     </div>
   );
 };
