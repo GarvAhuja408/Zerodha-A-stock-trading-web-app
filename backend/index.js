4779753c998eba2e228ccbcf1d4d349bd7614d47
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-08-10 23:44:58 +0530

    connecting database to express

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,26 @@
+require('dotenv').config();
+
 const express = require("express");
+const mongoose = require("mongoose");
 
-const app=express();
+const app = express();
 
-const port=3002;
+const PORT = process.env.PORT || 3002;
+const url = process.env.MONGO_URL;
 
-app.listen(port,()=>{
-    console.log("App is listening to port 3002");
-})
+async function main() {
+    await mongoose.connect(url);
+}
+
+main()
+    .then(() => {
+        console.log("Connected to DB");
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error:", err);
+    });
+    
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`);
+});
